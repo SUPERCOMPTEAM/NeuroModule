1888768cb3e31a0ff6cea9cd1ea1a499d2f8ae22
Pavloffff <pavlov.id.2003@gmail.com>
2024-04-03 00:22:25 +0300

    add module into docker && refactor names

--- docker/proxy.dockerfile
+++ docker/proxy.dockerfile
@@ -13,7 +13,7 @@ RUN git clone https://github.com/SUPERCOMPTEAM/SCT_nginx.git
 RUN mv /SCT_nginx /app
 WORKDIR /app
 
-RUN /app/auto/configure --with-http_ssl_module --with-mail --with-stream --with-stream_realip_module --with-cpp_test_module --with-cc=gcc --with-cpp=gcc --with-cc-opt="-fPIC" --with-cpu-opt=cpu --with-pcre --with-debug --with-http_perl_module
+RUN /app/auto/configure --with-http_ssl_module --with-mail --with-stream --with-stream_realip_module --with-cpp_test_module --with-cc=gcc --with-cpp=gcc --with-cc-opt="-fPIC" --with-cpu-opt=cpu --with-pcre --with-debug --with-http_perl_module --add-module=/app/ngx_http_upstream_sct_neuro_module 
 
 RUN make -j4
 RUN make install
--- ngx_http_upstream_sct_neuro_module/ngx_http_upstream_sct_neuro_module.c
+++ ngx_http_upstream_sct_neuro_module/ngx_http_upstream_sct_neuro_module.c
@@ -9,18 +9,18 @@
 #include <ngx_http.h>
 
 
-static ngx_int_t ngx_http_upstream_init_supercompteam_neuro_peer(ngx_http_request_t *r,
+static ngx_int_t ngx_http_upstream_init_sct_neuro_peer(ngx_http_request_t *r,
     ngx_http_upstream_srv_conf_t *us);
-static ngx_int_t ngx_http_upstream_get_supercompteam_neuro_peer(
+static ngx_int_t ngx_http_upstream_get_sct_neuro_peer(
     ngx_peer_connection_t *pc, void *data);
-static char *ngx_http_upstream_supercompteam_neuro(ngx_conf_t *cf, ngx_command_t *cmd,
+static char *ngx_http_upstream_sct_neuro(ngx_conf_t *cf, ngx_command_t *cmd,
     void *conf);
 
 
-static ngx_command_t  ngx_http_upstream_supercompteam_neuro_commands[] = {
-    { ngx_string("supercompteam_neuro"),    /* directive for using this module */
+static ngx_command_t  ngx_http_upstream_sct_neuro_commands[] = {
+    { ngx_string("sct_neuro"),    /* directive for using this module */
       NGX_HTTP_UPS_CONF|NGX_CONF_NOARGS,
-      ngx_http_upstream_supercompteam_neuro,
+      ngx_http_upstream_sct_neuro,
       0,
       0,
       NULL },
@@ -29,7 +29,7 @@ static ngx_command_t  ngx_http_upstream_supercompteam_neuro_commands[] = {
 };
 
 
-static ngx_http_module_t  ngx_http_upstream_supercompteam_neuro_module_ctx = {
+static ngx_http_module_t  ngx_http_upstream_sct_neuro_module_ctx = {
     NULL,                                  /* preconfiguration */
     NULL,                                  /* postconfiguration */
 
@@ -44,10 +44,10 @@ static ngx_http_module_t  ngx_http_upstream_supercompteam_neuro_module_ctx = {
 };
 
 
-ngx_module_t  ngx_http_upstream_supercompteam_neuro_module = {
+ngx_module_t  ngx_http_upstream_sct_neuro_module = {
     NGX_MODULE_V1,
-    &ngx_http_upstream_supercompteam_neuro_module_ctx,  /* module context */
-    ngx_http_upstream_supercompteam_neuro_commands,     /* module directives */
+    &ngx_http_upstream_sct_neuro_module_ctx,  /* module context */
+    ngx_http_upstream_sct_neuro_commands,     /* module directives */
     NGX_HTTP_MODULE,                                    /* module type */
     NULL,                                               /* init master */
     NULL,                                               /* init module */
@@ -61,53 +61,72 @@ ngx_module_t  ngx_http_upstream_supercompteam_neuro_module = {
 
 
 static ngx_int_t
-ngx_http_upstream_init_supercompteam_neuro(ngx_conf_t *cf,
+ngx_http_upstream_init_sct_neuro(ngx_conf_t *cf,
     ngx_http_upstream_srv_conf_t *us)
 {
     ngx_log_debug0(NGX_LOG_DEBUG_HTTP, cf->log, 0,
-                   "init least conn");
+                   "init sct neuro");
 
     if (ngx_http_upstream_init_round_robin(cf, us) != NGX_OK) {
         return NGX_ERROR;
     }
 
-    us->peer.init = ngx_http_upstream_init_supercompteam_neuro_peer;
+    us->peer.init = ngx_http_upstream_init_sct_neuro_peer;
 
     return NGX_OK;
 }
 
 
 static ngx_int_t
-ngx_http_upstream_init_supercompteam_neuro_peer(ngx_http_request_t *r,
+ngx_http_upstream_init_sct_neuro_peer(ngx_http_request_t *r,
     ngx_http_upstream_srv_conf_t *us)
 {
     ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
-                   "init least conn peer");
+                   "init sct neuro peer");
 
     if (ngx_http_upstream_init_round_robin_peer(r, us) != NGX_OK) {
         return NGX_ERROR;
     }
 
-    r->upstream->peer.get = ngx_http_upstream_get_supercompteam_neuro_peer;
+    r->upstream->peer.get = ngx_http_upstream_get_sct_neuro_peer;
 
     return NGX_OK;
 }
 
 
 static ngx_int_t
-ngx_http_upstream_get_supercompteam_neuro_peer(ngx_peer_connection_t *pc, void *data)
+ngx_http_upstream_get_sct_neuro_peer(ngx_peer_connection_t *pc, void *data)
 {
-    ngx_http_upstream_rr_peer_data_t  *rrp = data;
+    ngx_uint_t                    i;
+    ngx_http_upstream_rr_peer_t  *peer;
 
+    ngx_http_upstream_rr_peer_data_t  *rrp = data;
+    for (peer = rrp->peers->peer, i = 0;
+        peer;
+        peer = peer->next, i++)
+    {
+        ngx_log_debug8(NGX_LOG_DEBUG_HTTP, pc->log, 0,
+                   "peer no: %ui, socklen: %ui, name: %s, server: %s, "
+                   "current_weight: %d, effective_weight: %d, weight: %d, "
+                   "conns: %ui", i, peer->socklen, peer->name.data,
+                   peer->server.data, peer->current_weight, 
+                   peer->effective_weight, peer->weight, peer->conns);
+        ngx_log_debug8(NGX_LOG_DEBUG_HTTP, pc->log, 0,
+                   "max_conns: %ui, fails: %ui, accessed: %ld, checked: %ld"
+                   ", max_fails: %ui, fail_timeout: %ld, slow_start: %d, "
+                   "start_time: %d", peer->max_conns, peer->fails,
+                   peer->accessed, peer->checked, peer->max_fails, 
+                   peer->fail_timeout, peer->slow_start, peer->start_time);
+    }
     ngx_log_debug1(NGX_LOG_DEBUG_HTTP, pc->log, 0,
-                   "get peer, try: %ui", pc->tries);
+                   "get sct neuro peer, try: %ui", pc->tries);
     
     return ngx_http_upstream_get_round_robin_peer(pc, rrp);
 }
 
 
 static char *
-ngx_http_upstream_supercompteam_neuro(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+ngx_http_upstream_sct_neuro(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
     ngx_http_upstream_srv_conf_t  *uscf;
 
@@ -118,7 +137,7 @@ ngx_http_upstream_supercompteam_neuro(ngx_conf_t *cf, ngx_command_t *cmd, void *
                            "load balancing method redefined");
     }
 
-    uscf->peer.init_upstream = ngx_http_upstream_init_supercompteam_neuro;
+    uscf->peer.init_upstream = ngx_http_upstream_init_sct_neuro;
 
     uscf->flags = NGX_HTTP_UPSTREAM_CREATE
                   |NGX_HTTP_UPSTREAM_WEIGHT
